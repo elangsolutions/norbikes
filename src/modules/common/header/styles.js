231334faf7687a25851a80301abff878f9acb7f8
Eric <ericlang80@gmail.com>
2020-12-03 00:43:22 -0300

    several fixes

---
+++ src/modules/common/header/styles.js
@@ -0,0 +1,28 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import green from '@material-ui/core/colors/green';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: purple[500],
+        },
+        secondary: {
+            main: green[500],
+        },
+    },
+});
+
+const styles = {
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+};
+
+export default styles
---
+++ src/modules/landing/LandingPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../common/header/header";
+import Footer from "../common/footer/footer";
+
+
+const LandingPage = (props) => {
+
+    return (<>
+        <Header />
+        
+        <Footer />
+    </>)
+}
+
+export default LandingPage
