46d7eae323a32b05419ab39b45288275f70aedc5
Eric Lang <ericlang80@gmail.com>
2020-12-15 18:43:59 -0300

    testing out proto header

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
+const port = process.env.PORT || '8080';
+app.set('port', port);
+
 app.use(enforce.HTTPS({ trustProtoHeader: true }))
 
 http.createServer(app).listen(app.get('port'), function() {
