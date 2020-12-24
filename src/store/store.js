0221f26007a4ff76215a6c42e9765b4083ae305a
Eric Lang <ericlang80@gmail.com>
2020-12-24 10:37:36 -0300

    added some styles

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,28 @@
 import { createStore } from "redux";
 import rootReducer from "./reducers";
 
-export default createStore(rootReducer,  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export const loadState = (key) => {
+    try {
+        const serializedData = localStorage.getItem(key);
+        if (serializedData === null){
+            return undefined ;
+        }
+        return JSON.parse(serializedData)
+    } catch (error) {
+        console.log("Load Error",error)
+        return undefined;
+    }
+}
+
+export const saveState = (key,state) => {
+    try {
+        let serializedData = JSON.stringify(state)
+        localStorage.setItem(key, serializedData)
+    } catch (error) {
+        console.log("Save Error",error)
+    }
+}
+
+const initialData = loadState('state');
+
+export default createStore(rootReducer, initialData, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
